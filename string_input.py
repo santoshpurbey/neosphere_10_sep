a19b072361849fb4019ca7adcb621a168894c98e
Santosh Purbey <santosh.purbey@logpoint.com>
2018-09-17 07:51:44 +0545

    clear typo

--- string_input.py
+++ string_input.py
@@ -1,14 +1,14 @@
-name = input('Enter your name:') 
+name = input('Enter your name:')
 #Enter your name:Santosh
 #name
 #'Santosh'
-age = input('Enter your age:")
+age = input('Enter your age:')
 # Enter your age: 24
-print(type(a))
-print  'My name is {0} and age {1}'.format('Santosh', '24')
-My name is Santosh and age 24
+print(type(age))
+print('My name is {0} and age {1}'.format('Santosh', '24'))
+#My name is Santosh and age 24
 # Alternative approach
-print 'my name is {name} and age is {age}'.format(age=24, name='Santosh')
+print('my name is {name} and age is {age}'.format(age=24, name='Santosh'))
 
 name = input('Enter your name:')
 age = int(input("Enter yoiur age"))
